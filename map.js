9d47bfc45b440e5153e51aaa81b63e9f3bbdc437
Dean Sofer <ProLoser@users.noreply.github.com>
2024-08-03 23:10:47 -0700

    Update map.js

--- map.js
+++ map.js
@@ -55,18 +55,17 @@ async function initialize() {
         });
 
         // Add drop animation
-        const content = event.marker.content;
       
-        content.style.opacity = "0";
-        content.addEventListener("animationend", (event) => {
-          content.classList.remove("drop");
-          content.style.opacity = "1";
+        event.marker.content.style.opacity = "0";
+        event.marker.content.addEventListener("animationend", (event) => {
+          event.marker.content.classList.remove("drop");
+          event.marker.content.style.opacity = "1";
         });
       
         const time = 2 + Math.random(); // 2s delay for easy to see the animation
       
-        content.style.setProperty("--delay-time", time + "s");
-        intersectionObserver.observe(content);
+        event.marker.content.style.setProperty("--delay-time", time + "s");
+        intersectionObserver.observe(event.marker.content);
       });
 
       // Apply URL filters
@@ -135,12 +134,12 @@ window.filter = function (filters = {}) {
       event.visible = false;
     }
 
-    event.marker.style.opacity = event.visible ? 1 : 0;
-    // if (event.visible) {
-    //   event.marker.classList.add('drop')
-    // } else {
-    //   event.marker.style.opacity = '0';
-    // }
+    // event.marker.content.style.opacity = event.visible ? 1 : 0;
+    if (event.visible) {
+      event.marker.content.classList.add('drop')
+    } else {
+      event.marker.content.style.opacity = '0';
+    }
     // event.marker.style.display = event.visible ? 'block' : 'none';
     
     return event.visible;
