8db70d8737a2ea319643dc4d093503da00861bf9
Dean Sofer <Dean.Sofer@sony.com>
2025-08-12 23:27:47 -0700

    Reset button and moved controls into map

--- map.js
+++ map.js
@@ -58,17 +58,18 @@ async function initialize() {
       Events.infoWindow().close();
   });
 
-if (navigator.geolocation) {
-  const locationButton = document.createElement("button");
-  locationButton.classList.add("ui-button");
-  locationButton.id = "myLocation";
-  locationButton.title = "My Location";
-  locationButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"></path>
-  </svg>`;
-  locationButton.addEventListener("click", showUserLocation);
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationButton);
-}
+  // Add "You Are Here" button
+  if (navigator.geolocation) {
+    const locationButton = document.createElement("button");
+    locationButton.classList.add("ui-button");
+    locationButton.id = "myLocation";
+    locationButton.title = "My Location";
+    locationButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"></path>
+    </svg>`;
+    locationButton.addEventListener("click", showUserLocation);
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationButton);
+  }
 
   console.log('Loading Events...');
   window.events.load()
@@ -133,6 +134,9 @@ if (navigator.geolocation) {
         }
       });
       const form = document.getElementById('controls');
+      form.addEventListener('reset', window.filter);
+      map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(form);
+      map.controls[google.maps.ControlPosition.LEFT_TOP].push(document.getElementById('feedback'));
       // Update the date picker
       if (minDate && maxDate) {
         form.elements['date'].min = minDate.toLocaleDateString('fr-ca');
