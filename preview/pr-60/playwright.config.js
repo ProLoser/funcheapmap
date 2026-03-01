25868d41ab30916b483577c816f9b83a15906934
ProLoser <ProLoser@users.noreply.github.com>
2026-03-01 06:21:54 +0000

    Remove preview for PR 60 🛬

--- preview/pr-60/playwright.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testDir: 'tests',
-  timeout: 30000,
-  use: {
-    baseURL: 'http://localhost:8000',
-    trace: 'on-first-retry'
-  },
-  projects: [{ name: 'chromium', use: { browserName: 'chromium' } }]
-}
