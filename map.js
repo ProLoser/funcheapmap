0398653bec4f80c9d5915d55d11002d6baa31447
Dean Sofer <Dean.Sofer@sony.com>
2026-02-28 23:11:31 -0800

    Update endpoint URLs

--- map.js
+++ map.js
@@ -451,7 +451,7 @@ window.showUserLocation = async function () {
  */
 class Events {
   static API_TOKEN = '__APIFY_TOKEN__';
-  static API = `https://api.apify.com/v2/acts/apify~cheerio-scraper/runs/last/dataset/items?clean=true&token=${Events.API_TOKEN}`;
+  static API = `https://api.apify.com/v2/actor-tasks/proloser~funcheapsf-cheerio-scraper-task/runs/last/dataset/items?token=${Events.API_TOKEN}`;
   /**
    * Generates and returns a google maps info window
    * 
